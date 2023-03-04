Fixed a bug in softmax.

student-policy/policy.py
@@ -4,12 +4,12 @@ class Policy(torch.nn.Module):
 
   def __init__(self, state_space, action_space):
     super(Policy, self).__init__()
-    width = 64
+    width = 16
     self.lstm = torch.nn.LSTM(input_size = state_space, hidden_size = width, batch_first = True)
     self.project_to_output = torch.nn.Sequential(
-      torch.nn.Linear(width, action_space, bias=False),
-      torch.nn.Softmax()
+      torch.nn.Dropout(),
+      torch.nn.Linear(width, action_space, bias=False)
     )
 
   def forward(self, x):
-    return self.project_to_output(self.lstm(x)[0])
+    return torch.nn.functional.softmax(self.project_to_output(self.lstm(x)[0]), dim=2)

student-policy/train.py
@@ -14,7 +14,7 @@ print(observations.shape)
 SPLIT=3000
 training_observations = observations[:SPLIT]
 training_actions = actions[:SPLIT]
-VALIDATION_SIZE = 999
+VALIDATION_SIZE = 1000
 validation_observations = observations[SPLIT:SPLIT+VALIDATION_SIZE]
 validation_actions = actions[SPLIT:SPLIT+VALIDATION_SIZE]
 
@@ -22,27 +22,30 @@ writer = SummaryWriter()
 
 policy = Policy(4, 2)
 
-optimizer = torch.optim.Adam(policy.parameters(), lr=1e-1)
+optimizer = torch.optim.Adam(policy.parameters(), lr=1e-2)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3000, gamma=0.5)
 
-# It's a tiny network and a tiny problem so we don't need to do anything fancy.
+NUMBER_OF_STEPS = 2
+
+# It's a tiny network and a tiny problem so we don't need to do anything fancy
+# except stop before it start overfitting.
 STEPS = 100000
 for step in range(STEPS):
-  training_observations = training_observations.reshape([int(SPLIT / 3), 3, -1])
+  training_observations = training_observations.reshape([int(SPLIT / NUMBER_OF_STEPS), NUMBER_OF_STEPS, -1])
   model_actions = policy(torch.as_tensor(training_observations, dtype=torch.float32))
   teacher_actions = torch.as_tensor(training_actions, dtype=torch.long)
   # Three time indices. The data is sequential. Occasionally this might hit a split in the episodes,
   # but we don't care as the chatbot generally also sees the history across episodes.
-  teacher_actions = teacher_actions.reshape([int(SPLIT / 3), 3])
+  teacher_actions = teacher_actions.reshape([int(SPLIT / NUMBER_OF_STEPS), NUMBER_OF_STEPS])
   loss = torch.sum(torch.square(model_actions - torch.nn.functional.one_hot(teacher_actions, 2)))
   optimizer.zero_grad()
   loss.backward()
   optimizer.step()
 
-  validation_observations = validation_observations.reshape([int(VALIDATION_SIZE / 3), 3, -1])
+  validation_observations = validation_observations.reshape([int(VALIDATION_SIZE / NUMBER_OF_STEPS), NUMBER_OF_STEPS, -1])
   validation_model_actions = policy(torch.as_tensor(validation_observations, dtype=torch.float32))
   validation_teacher_actions = torch.as_tensor(validation_actions, dtype=torch.long)
-  validation_teacher_actions = validation_teacher_actions.reshape([int(VALIDATION_SIZE / 3), 3])
+  validation_teacher_actions = validation_teacher_actions.reshape([int(VALIDATION_SIZE / NUMBER_OF_STEPS), NUMBER_OF_STEPS])
 
   validation_loss = torch.sum(torch.square(validation_model_actions - torch.nn.functional.one_hot(validation_teacher_actions, 2)))
 